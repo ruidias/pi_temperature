9d376b4f6b1dc1208fd9ad966180a3c95fb35703
ruidias <ruidias83@gmail.com>
2017-06-28 18:28:23 +0100

    Update station.py

--- src/station.py
+++ src/station.py
@@ -1,80 +1,64 @@
+#!/usr/bin/env python
+#
+# GrovePi Example for using the Grove Temperature Sensor (http://www.seeedstudio.com/wiki/Grove_-_Temperature_Sensor)
+#
+# The GrovePi connects the Raspberry Pi and Grove sensors.  You can learn more about GrovePi here:  http://www.dexterindustries.com/GrovePi
+#
+# Have a question about this example?  Ask on the forums here:  http://forum.dexterindustries.com/c/grovepi
+#
+'''
+## License
+
+The MIT License (MIT)
+
+GrovePi for the Raspberry Pi: an open source platform for connecting Grove Sensors to the Raspberry Pi.
+Copyright (C) 2017  Dexter Industries
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+# NOTE: 
+# 	The sensor uses a thermistor to detect ambient temperature.
+# 	The resistance of a thermistor will increase when the ambient temperature decreases.
+#	
+# 	There are 3 revisions 1.0, 1.1 and 1.2, each using a different model thermistor.
+# 	Each thermistor datasheet specifies a unique Nominal B-Constant which is used in the calculation forumla.
+#	
+# 	The second argument in the grovepi.temp() method defines which board version you have connected.
+# 	Defaults to '1.0'. eg.
+# 		temp = grovepi.temp(sensor)        # B value = 3975
+# 		temp = grovepi.temp(sensor,'1.1')  # B value = 4250
+# 		temp = grovepi.temp(sensor,'1.2')  # B value = 4250
+
 import time
-import math
-import grove_i2c_adc
-import Adafruit_BBIO.GPIO as GPIO
+import grovepi
 
-adc = grove_i2c_adc.I2cAdc()
+# Connect the Grove Temperature Sensor to analog port A0
+# SIG,NC,VCC,GND
+sensor = 0
 
-def read_temperature(model = 'v1.2'):
-    "Read temperature values in Celsius from Grove Temperature Sensor"
-    # each of the sensor revisions use different thermistors, each with their own B value constant
-    if model == 'v1.2':
-        bValue = 3975  # sensor v1.2 uses thermistor ??? (assuming NCP18WF104F03RC until SeeedStudio clarifies)
-    elif model == 'v1.1':
-        bValue = 4250  # sensor v1.1 uses thermistor NCP18WF104F03RC
-    elif model == 'v1.0':
-        bValue = 3975  # sensor v1.0 uses thermistor TTC3A103*39H
-    elif model == 'hts':   
-        bValue = 3975  # High Temperature Sensor uses thermistor TTC3A103*39H
-        
-    total_value = 0
-    for index in range(20):
-        sensor_value = adc.read_adc()
-        total_value += sensor_value
-        time.sleep(0.05)
-    average_value = float(total_value / 20)
+while True:
+    try:
+        temp = grovepi.temp(sensor,'1.1')
+        print("temp =", temp)
+        time.sleep(.5)
 
-    #print "average_value", average_value
-    print "model", model
-    
-    # Transform the ADC data into the data of Arduino platform.
-    sensor_value_tmp = (float)(average_value / 4095 * 2.95 * 2 / 3.3 * 1023)
-    print "sensor_value_tmp", sensor_value_tmp
-    resistance = (float)(1023 - sensor_value_tmp) * 10000 / sensor_value_tmp
-    temperature = round((float)(1 / (math.log(resistance / 10000) / bValue + 1 / 298.15) - 273.15), 2)
-    return temperature
-
-# Hardware: Grove - I2C ADC, Grove - Temperature Sensor
-# Connect the Grove - I2C ADC to I2C Grove port of Beaglebone Green, and then connect the Grove - Temperature Sensor to Grove - I2C ADC.
-if __name__ == '__main__':
-
-    while True:
-        try:
-            # Read temperature values in Celsius from Grove Temperature Sensor
-            temperature = read_temperature('v1.2')
-            
-            print "temperature = ", temperature
-    
-            GPIO.setup("USR0", GPIO.OUT) 
-            GPIO.setup("USR1", GPIO.OUT)
-            GPIO.setup("USR2", GPIO.OUT)
-            GPIO.setup("USR3", GPIO.OUT) 
-   
-            GPIO.output("USR0", GPIO.LOW)
-            GPIO.output("USR1", GPIO.LOW)
-            GPIO.output("USR2", GPIO.LOW)
-            GPIO.output("USR3", GPIO.LOW)
-                
-            if temperature >= 28:
-                GPIO.output("USR3", GPIO.HIGH)
-                GPIO.output("USR2", GPIO.HIGH)
-                GPIO.output("USR1", GPIO.HIGH)
-                GPIO.output("USR0", GPIO.HIGH)
-            elif temperature >= 26:
-                GPIO.output("USR3", GPIO.LOW)                
-                GPIO.output("USR2", GPIO.HIGH)
-                GPIO.output("USR1", GPIO.HIGH)
-                GPIO.output("USR0", GPIO.HIGH)                
-            elif temperature >= 24:
-                GPIO.output("USR3", GPIO.LOW)                
-                GPIO.output("USR2", GPIO.LOW)
-                GPIO.output("USR1", GPIO.HIGH)
-                GPIO.output("USR0", GPIO.HIGH)               
-            elif temperature >= 22:
-                GPIO.output("USR3", GPIO.LOW)                
-                GPIO.output("USR2", GPIO.LOW)
-                GPIO.output("USR1", GPIO.LOW)
-                GPIO.output("USR0", GPIO.HIGH)                
-
-        except IOError:
-            print "Error"
+    except KeyboardInterrupt:
+        break
+    except IOError:
+        print ("Error")
